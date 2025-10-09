cd8986272c86ef1d20bac226f2574344320594b1
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-09 08:40:28 +0530

    Initial commit - frontend and empty backend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import {Toaster} from "react-hot-toast";
+import Footer from './components/Footer';
+import Login from './components/Login';
+import { useAppContext } from './context/AppContext';
+import AllProducts from './pages/AllProducts';
+import ProductCategory from './pages/ProductCategory';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+
+const App = () => {
+
+  const isSellerPath= useLocation().pathname.includes("seller");
+  const {showUserLogin} = useAppContext()
+
+  return (
+    <div>
+     {isSellerPath ? null: <Navbar/>} 
+     {showUserLogin ? <Login/> : null}
+
+     <Toaster/>
+      <div className=  {`${isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32" }` } >
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/products' element={<AllProducts/>}/>
+          <Route path='/products/:category' element={<ProductCategory/>}/>
+          <Route path='/products/:category/:id' element={<ProductDetails/>}/> 
+          <Route path='/Cart' element={<Cart/>}/> 
+          
+        </Routes>
+      </div>
+      <div>
+        {!isSellerPath && <Footer/>}
+      </div>
+
+    </div>
+  )
+}
+
+export default App
