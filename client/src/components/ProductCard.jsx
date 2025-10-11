836731794aa69dddce8931abb9ab0ae7a3c7aa56
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-11 18:26:17 +0530

    complete-myOrder-page

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -25,7 +25,7 @@ const ProductCard = ({product}) => {
                 </div>
                 <div className="flex items-end justify-between mt-3">
                     <p className="md:text-xl text-base font-medium text-green-400">
-                       {currency} ${product.offerPrice} {" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}${product.price}</span>
+                       {currency}{product.offerPrice} {" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}{product.price}</span>
                     </p>
                     <div onClick={(e)=>{e.stopPropagation();}} className="text-red-600">
                         {!cartItems[product._id] ? (
