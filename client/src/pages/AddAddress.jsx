5a6be16981499795d94ff50cb563dd5b5c3bcdf5
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-11 15:24:35 +0530

    complete-add-address-page

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -37,7 +37,7 @@ const handleChange = (e) => {
 
   setAddress((prevAddress)=>({
 
-    ...AddAddress,
+    ...prevAddress,
     [name]:value,
 
   }))
@@ -56,13 +56,42 @@ const handleChange = (e) => {
         <div className='flex-1 max-w-md'>
           <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'>
 
-            <div>
+            <div className='grid grid-cols-2 gap-4'>
                <InputField handleChange={handleChange} address={address}
                 name='firstName' type="text" placeholder="First Name"/>
                 <InputField handleChange={handleChange} address={address}
                 name='lastName' type="text" placeholder="Last Name"/>
 
             </div>
+
+                <InputField handleChange={handleChange} address={address}
+                name='email' type="email" placeholder="Email address"/>
+                <InputField handleChange={handleChange} address={address}
+                name='street' type="text" placeholder="Street"/>
+
+
+             <div className='grid grid-cols-2 gap-4'>
+               <InputField handleChange={handleChange} address={address}
+                name='city' type="text" placeholder="City"/>
+                <InputField handleChange={handleChange} address={address}
+                name='state' type="text" placeholder="State"/>
+             </div>
+             
+             <div className='grid grid-cols-2 gap-4'>
+               <InputField handleChange={handleChange} address={address}
+                name='zipcode' type="number" placeholder="Zip code"/>
+
+               <InputField handleChange={handleChange} address={address}
+                name='phone' type="text" placeholder="Phone"/> 
+                
+             </div>
+
+             
+
+            <button className='w-full mt-g bg-primary text-white py-3 hover:bg-primary-dull transition cursor-pointer uppercase'>
+              Save Address
+            </button>
+
           </form>
         </div>
         <img className='md:mr-16 mb-16 md:mt-0' src={assets.add_address_iamge} alt="Add Address" />
