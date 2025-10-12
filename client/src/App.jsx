03eb4959e6b0a372b76f631882cf9584725dfc01
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-12 12:16:56 +0530

    create-seller-login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,11 +12,12 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
+import SellerLogin from './components/seller/SellerLogin';
 
 const App = () => {
 
   const isSellerPath= useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext()
+  const {showUserLogin,isSeller} = useAppContext()
 
   return (
     <div>
@@ -33,6 +34,7 @@ const App = () => {
           <Route path='/Cart' element={<Cart/>}/> 
           <Route path='/add-address' element={<AddAddress/>}/> 
           <Route path='/my-orders' element={<MyOrders/>}/> 
+          <Route path='/seller' element={isSeller ? null : <SellerLogin/>}/> 
           
         </Routes>
       </div>
