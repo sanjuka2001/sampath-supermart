26c3a5df395b80f358b1c2bc3e9b55784f177c2a
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-14 18:21:58 +0530

    created-user- register-and-login-api

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -17,7 +18,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin:allowedOrigins,credentials:true}));
 
+
 app.get('/', (req,res)=> res.send("API is Working"));
+app.use('/api/user',userRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
