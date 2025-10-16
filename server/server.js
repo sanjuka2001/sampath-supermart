14c58fac958d795358f3bbc3b715c0a5edaad4a9
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-16 20:20:20 +0530

    feat: add cart route and controller integration

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
 import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -25,6 +27,8 @@ app.use(cors({origin:allowedOrigins,credentials:true}));
 app.get('/', (req,res)=> res.send("API is Working"));
 app.use('/api/user',userRouter)
 app.use('/api/seller',sellerRouter)
+app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
