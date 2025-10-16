86e83dae4811670eb78456c1ad8cb295af6fd024
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-16 16:54:29 +0530

    completed-add-id-stock-routes

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import { upload } from '../configs/multer';
+import authSeller from '../middlewares/authSeller';
+import { addProduct, changeStock, productById, productList } from '../controllers/productController';
 
 const productRouter = express.Router();
 
-productRouter
+productRouter.post('/add',upload.array([images]),authSeller,addProduct);
+productRouter.get('/list', productList);
+productRouter.get('/id',productById);
+productRouter.post('/stock',authSeller, changeStock);
+
+export default productRouter;
