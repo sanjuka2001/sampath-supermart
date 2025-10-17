e4a3cbf9dbd553798a6a2cb03011165335b665e8
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-17 19:58:56 +0530

    Fix productRoute upload field and import extensions

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { upload } from '../configs/multer';
-import authSeller from '../middlewares/authSeller';
-import { addProduct, changeStock, productById, productList } from '../controllers/productController';
+import { upload } from '../configs/multer.js';
+import authSeller from '../middlewares/authSeller.js';
+import { addProduct, changeStock, productById, productList } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add',upload.array([images]),authSeller,addProduct);
+productRouter.post('/add',upload.array("images"),authSeller,addProduct);
 productRouter.get('/list', productList);
 productRouter.get('/id',productById);
 productRouter.post('/stock',authSeller, changeStock);
