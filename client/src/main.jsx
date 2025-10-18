1191e61cd871e1d5c8d66267d50d293eac646861
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-18 12:28:29 +0530

    import toaster to main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
    <AppContextProvider>
     <App /> 
+    <Toaster/>
    </AppContextProvider>
     
   </BrowserRouter>,
