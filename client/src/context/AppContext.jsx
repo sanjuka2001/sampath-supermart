9641e60c9d468c836962f050af571b18e726149f
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-19 16:31:34 +0530

    updated database cart items

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -146,6 +146,27 @@ const fetchUser = async ()=> {
         
     },[])
 
+    //Update Database Cart Items
+
+    useEffect(()=>{
+
+        const updateCart = async () => {
+            try {
+                const {data} = await axios.post('/api/cart/update', {cartItems})
+                if(!data.success){
+                   toast.error(data.message)
+                }
+
+            } catch ( error) {
+                   toast.error(error.message)
+            }
+            
+        }
+        if(user){
+                updateCart()
+            }
+    },[cartItems])
+
 
     const value = {navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,
         products,currency,addToCart,updateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery,
