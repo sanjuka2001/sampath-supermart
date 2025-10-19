9a75f56c32e5e09752370088f1b6f2e73936a172
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-19 16:30:47 +0530

    updated authUser req.body

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -10,6 +10,7 @@ const authUser = async (req, res, next) => {
   try {
     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
     if (tokenDecode.id) {
+        req.body = req.body || {};
        req.body.userId = tokenDecode.id;
     } else {
       return res.json({ success: false, message: "Not Authorized" });
