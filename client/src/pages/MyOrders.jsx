2447fffd8d1a1a9b8d7caa3e36797761861fcdda
NWKNS Thilakarathna <nipunsanjukathilakarathna@gmail.com>
2025-10-20 14:57:18 +0530

    feat(my-orders): integrate API call to fetch user orders and add user check

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -6,15 +6,27 @@ import { dummyOrders } from '../assets/assets'
 const MyOrders = () => {
 
    const [myOrders,setMyOrders] = useState([])
-   const {currency} = useAppContext()
+   const {currency,axios,user} = useAppContext()
 
    const fetchMyOrders = async () => {
-       setMyOrders(dummyOrders)
+      try {
+        const {data} =await axios.get('/api/order/user')
+        if(data.success){
+          setMyOrders(data.orders)  
+        }
+      } catch (error) {
+        console.log(error);
+        
+      }
+       
    }
 
    useEffect(()=>{
-     fetchMyOrders()
-   },[])
+      if(user){
+         fetchMyOrders()
+      }
+    
+   },[user])
 
 
     return (
